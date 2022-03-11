4e62ca858f15174f1e03fb35a13a09bea8c96130
frantastic24 <95725102+frantastic24@users.noreply.github.com>
2022-03-11 20:50:52 -0300

    lua flxtrail WOOO YEA

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -965,6 +965,26 @@ class FunkinLua {
 			PlayState.instance.modchartSprites.set(tag, leSprite);
 			leSprite.active = true;
 		});
+		Lua_helper.add_callback(lua, "addCharacterTrail", function(target:String, length:Int = 4, delay:Int = 24, alpha:Float = 0.3, diff:Float = 0.069) {
+			switch(target.toLowerCase()) {
+				case 'dad' | 'opponent':
+                    var trail = new FlxTrail(PlayState.instance.dad, null, length, delay, alpha, diff);
+					if(PlayState.instance.dad != null) PlayState.instance.insert(PlayState.instance.members.indexOf(PlayState.instance.dadGroup) - 1, trail);
+				case 'gf' | 'girlfriend':
+					var trail = new FlxTrail(PlayState.instance.gf, null, length, delay, alpha, diff);
+					if(PlayState.instance.gf != null) PlayState.instance.insert(PlayState.instance.members.indexOf(PlayState.instance.gfGroup) - 1, trail);
+				default:
+                     var trail = new FlxTrail(PlayState.instance.boyfriend, null, length, delay, alpha, diff);
+					if(PlayState.instance.boyfriend != null) PlayState.instance.insert(PlayState.instance.members.indexOf(PlayState.instance.boyfriendGroup) - 1, trail);
+			}
+		});
+		Lua_helper.add_callback(lua, "addSpriteTrail", function(obj:String, length:Int = 4, delay:Int = 24, alpha:Float = 0.3, diff:Float = 0.069) {
+			var object:ModchartSprite = PlayState.instance.modchartSprites.get(obj);
+			if(object != null) {
+				var trail = new FlxTrail(PlayState.instance.modchartSprites.get(obj), null, length, delay, alpha, diff);
+				getInstance().insert(getInstance().members.indexOf(PlayState.instance.modchartSprites.get(obj)) - 1, trail);
+			}
+		});
 		Lua_helper.add_callback(lua, "makeAnimatedLuaSprite", function(tag:String, image:String, x:Float, y:Float, ?spriteType:String = "sparrow") {
 			tag = tag.replace('.', '');
 			resetSpriteTag(tag);
