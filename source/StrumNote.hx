71c9d26d34edce195cb41e5670198ba8b21999bd
frantastic24 <95725102+frantastic24@users.noreply.github.com>
2022-08-27 00:17:09 -0300

    simplify strum animation code

--- source/StrumNote.hx
+++ source/StrumNote.hx
@@ -14,6 +14,9 @@ class StrumNote extends FlxSprite
 	public var direction:Float = 90;//plan on doing scroll directions soon -bb
 	public var downScroll:Bool = false;//plan on doing scroll directions soon -bb
 	public var sustainReduce:Bool = true;
+
+	private var colArray:Array<String> = ['purple', 'blue', 'green', 'red'];
+	private var dirArray:Array<String> = ['LEFT', 'DOWN', 'UP', 'RIGHT'];
 	
 	private var player:Int;
 	
@@ -55,61 +58,23 @@ class StrumNote extends FlxSprite
 
 			antialiasing = false;
 			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
-
-			animation.add('green', [6]);
-			animation.add('red', [7]);
-			animation.add('blue', [5]);
-			animation.add('purple', [4]);
-			switch (Math.abs(noteData) % 4)
-			{
-				case 0:
-					animation.add('static', [0]);
-					animation.add('pressed', [4, 8], 12, false);
-					animation.add('confirm', [12, 16], 12, false);
-				case 1:
-					animation.add('static', [1]);
-					animation.add('pressed', [5, 9], 12, false);
-					animation.add('confirm', [13, 17], 12, false);
-				case 2:
-					animation.add('static', [2]);
-					animation.add('pressed', [6, 10], 12, false);
-					animation.add('confirm', [14, 18], 12, false);
-				case 3:
-					animation.add('static', [3]);
-					animation.add('pressed', [7, 11], 12, false);
-					animation.add('confirm', [15, 19], 12, false);
-			}
+			
+			animation.add(colArray[noteData], [noteData + 4]);
+			animation.add('static', [noteData]);
+			animation.add('pressed', [noteData + 4, noteData + 8], 12, false);
+			animation.add('confirm', [noteData + 12, noteData + 16], 12, false);
 		}
 		else
 		{
 			frames = Paths.getSparrowAtlas(texture);
-			animation.addByPrefix('green', 'arrowUP');
-			animation.addByPrefix('blue', 'arrowDOWN');
-			animation.addByPrefix('purple', 'arrowLEFT');
-			animation.addByPrefix('red', 'arrowRIGHT');
+			animation.addByPrefix(colArray[noteData], 'arrow' + dirArray[noteData]);
 
 			antialiasing = ClientPrefs.globalAntialiasing;
 			setGraphicSize(Std.int(width * 0.7));
 
-			switch (Math.abs(noteData) % 4)
-			{
-				case 0:
-					animation.addByPrefix('static', 'arrowLEFT');
-					animation.addByPrefix('pressed', 'left press', 24, false);
-					animation.addByPrefix('confirm', 'left confirm', 24, false);
-				case 1:
-					animation.addByPrefix('static', 'arrowDOWN');
-					animation.addByPrefix('pressed', 'down press', 24, false);
-					animation.addByPrefix('confirm', 'down confirm', 24, false);
-				case 2:
-					animation.addByPrefix('static', 'arrowUP');
-					animation.addByPrefix('pressed', 'up press', 24, false);
-					animation.addByPrefix('confirm', 'up confirm', 24, false);
-				case 3:
-					animation.addByPrefix('static', 'arrowRIGHT');
-					animation.addByPrefix('pressed', 'right press', 24, false);
-					animation.addByPrefix('confirm', 'right confirm', 24, false);
-			}
+			animation.addByPrefix('static', 'arrow' + dirArray[noteData]);
+			animation.addByPrefix('pressed', dirArray[noteData].toLowerCase() + ' press', 24, false);
+			animation.addByPrefix('confirm', dirArray[noteData].toLowerCase() + ' confirm', 24, false);
 		}
 		updateHitbox();
 
