8f5e5d6a900de80fcc95bae10ad5b7c756407c3a
frantastic24 <95725102+frantastic24@users.noreply.github.com>
2023-07-08 20:13:46 -0300

    forgor about note array stuff

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -70,21 +70,59 @@ class StrumNote extends FlxSprite
 			antialiasing = false;
 			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
 			
-			animation.add(colArray[noteData], [noteData + 4]);
-			animation.add('static', [noteData]);
-			animation.add('pressed', [noteData + 4, noteData + 8], 12, false);
-			animation.add('confirm', [noteData + 12, noteData + 16], 12, false);
+			animation.add('green', [6]);
+			animation.add('red', [7]);
+			animation.add('blue', [5]);
+			animation.add('purple', [4]);
+			switch (Math.abs(noteData) % 4)
+			{
+				case 0:
+					animation.add('static', [0]);
+					animation.add('pressed', [4, 8], 12, false);
+					animation.add('confirm', [12, 16], 24, false);
+				case 1:
+					animation.add('static', [1]);
+					animation.add('pressed', [5, 9], 12, false);
+					animation.add('confirm', [13, 17], 24, false);
+				case 2:
+					animation.add('static', [2]);
+					animation.add('pressed', [6, 10], 12, false);
+					animation.add('confirm', [14, 18], 12, false);
+				case 3:
+					animation.add('static', [3]);
+					animation.add('pressed', [7, 11], 12, false);
+					animation.add('confirm', [15, 19], 24, false);
+			}
 		}
 		else
 		{
 			frames = Paths.getSparrowAtlas(texture);
-			animation.addByPrefix(colArray[noteData], 'arrow' + dirArray[noteData]);
+			animation.addByPrefix('green', 'arrowUP');
+			animation.addByPrefix('blue', 'arrowDOWN');
+			animation.addByPrefix('purple', 'arrowLEFT');
+			animation.addByPrefix('red', 'arrowRIGHT');
 
 			setGraphicSize(Std.int(width * 0.7));
 
-			animation.addByPrefix('static', 'arrow' + dirArray[noteData]);
-			animation.addByPrefix('pressed', dirArray[noteData].toLowerCase() + ' press', 24, false);
-			animation.addByPrefix('confirm', dirArray[noteData].toLowerCase() + ' confirm', 24, false);
+			switch (Math.abs(noteData) % 4)
+			{
+				case 0:
+					animation.addByPrefix('static', 'arrowLEFT');
+					animation.addByPrefix('pressed', 'left press', 24, false);
+					animation.addByPrefix('confirm', 'left confirm', 24, false);
+				case 1:
+					animation.addByPrefix('static', 'arrowDOWN');
+					animation.addByPrefix('pressed', 'down press', 24, false);
+					animation.addByPrefix('confirm', 'down confirm', 24, false);
+				case 2:
+					animation.addByPrefix('static', 'arrowUP');
+					animation.addByPrefix('pressed', 'up press', 24, false);
+					animation.addByPrefix('confirm', 'up confirm', 24, false);
+				case 3:
+					animation.addByPrefix('static', 'arrowRIGHT');
+					animation.addByPrefix('pressed', 'right press', 24, false);
+					animation.addByPrefix('confirm', 'right confirm', 24, false);
+			}
 		}
 		updateHitbox();
 
